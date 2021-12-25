7198d3a9b296bed9f7e74539a7daca624b46805b
Kévin Gatel <kevin.gatel@insa'rouen.fr>
2021-12-25 21:35:16 +0100

    thib

--- Code/Tricount.java
+++ Code/Tricount.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public interface Tricount extends Remote {
 	public void GetEquilibre(Personne[] Pers) throws RemoteException;
-	public void AddDepense(int id, String Com, int acheteur, float Val, int[] receveur, int nb_receveur) throws RemoteException;
+	public void AddDepense(int id, String Com, int acheteur, double Val, int[] receveur) throws RemoteException;
 	public boolean Connexion (String prenom) throws RemoteException;
 	public ArrayList<Personne> GetParticipants() throws RemoteException;
 	public ArrayList<Depense> GetDepense() throws RemoteException;
