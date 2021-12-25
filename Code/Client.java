687525bbc5bbce9440efdd669a626bacbf5cf51a
Thibaut <thibaut.andregallis@insa-rouen.fr>
2021-12-25 21:20:48 +0100

    choix du prenom dans client

--- Code/Client.java
+++ Code/Client.java
@@ -3,7 +3,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
-
+import java.util.Scanner;
 import javax.swing.SwingUtilities;
 
 public class Client {
@@ -11,7 +11,18 @@ public class Client {
 	public static void main(String args[]) throws RemoteException {
 		String machine = "localhost";
 	    int port = 1099;
-	    Personne stubPers, Pers = new PersonneImpl("Thibaut");
+	    Personne Pers;
+	    if (args.length==1) {
+		    Pers = new PersonneImpl(args[0]);
+	    } else {
+	    	Scanner sc = new Scanner(System.in);
+	    	System.out.println("Donnez votre nom ");
+	    	String prenom;
+	    	prenom = sc.nextLine();
+	    	sc.close();
+		    Pers = new PersonneImpl(prenom);
+	    }
+	    Personne stubPers;
 	    try {
 	    	Registry registry = LocateRegistry.getRegistry(machine, port);
 	    	Tricount obj = (Tricount)registry.lookup("Tricountdesfamilles");
